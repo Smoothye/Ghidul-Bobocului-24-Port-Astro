b650c84d5b98c6263c598a65e97bfc6a1893394f
Petrut Raul <raul.petrut44@yahoo.com>
2024-09-11 14:03:56 +0300

    Almost finished port, need to make welcome page transition and to check for irregularities

---
+++ src/scripts/bookAnimation.js
@@ -0,0 +1,104 @@
+var bookWrapper = document.getElementById("book-wrapper");
+var prevButton = document.getElementById("prev");
+var nextButton = document.getElementById("next");
+var butonCuprins = document.getElementById("buton-cuprins");
+
+var numberOfPages = 5;
+var numberOfPagesNarrow = 2 * numberOfPages;
+
+var currentPage = 1;
+var currentPageNarrow = 1;
+
+var pages = [];
+var pagesNarrow = [];
+for(let i = 1; i <= numberOfPages; i++) {
+    
+    pages[i] = document.getElementById(`page${i}`);
+    pages[i].style.zIndex = `${numberOfPages - i + 1}`;
+
+    pagesNarrow[i] = document.getElementById(`page${i}-narrow`);
+    pagesNarrow[i].style.zIndex = `${numberOfPages - i + 1}`;
+}
+for(let i = numberOfPages + 1; i <= 2 * numberOfPages; i++) {
+
+    pagesNarrow[i] = document.getElementById(`page${i}-narrow`);
+    pagesNarrow[i].style.zIndex = `${numberOfPages - i + 1}`; 
+}
+
+var nextZIndex = numberOfPages;
+
+function animatie(id) { // -1 = previous page | 1 = next page
+
+    if(id === -1) { // previous page
+        if(currentPage === 1) return;
+        
+        if(currentPage === numberOfPages + 1) {
+
+            bookWrapper.style.transform = "translateX(50%)";
+            nextButton.style.display = "inline";
+        }
+
+        currentPage--;
+
+        if(currentPage === 1) {
+
+            bookWrapper.style.transform = "translateX(0%)";
+            prevButton.style.display = "none";
+        }
+
+        pages[currentPage].classList.remove("flipped");
+        pages[currentPage].style.zIndex = `${nextZIndex}`;
+        nextZIndex++;
+    }
+    else { // next page
+        if(currentPage === numberOfPages + 1) return;
+
+        pages[currentPage].classList.add("flipped");
+        pages[currentPage].style.zIndex = `${nextZIndex}`;
+        nextZIndex++;
+
+        if(currentPage === 1) {
+
+            bookWrapper.style.transform = "translateX(50%)";
+            prevButton.style.display = "inline";
+        }
+        
+        currentPage++;
+
+        if(currentPage === numberOfPages + 1) {
+
+            bookWrapper.style.transform = "translateX(100%)";
+            nextButton.style.display = "none";
+        }
+    }
+
+    butonCuprins.style.left = (currentPage <= 3 ? "0px" : "-100px");
+}
+
+function goToPage(pageNumber) {
+
+    while(currentPage != pageNumber)
+        animatie(currentPage < pageNumber ? 1 : -1);
+}
+
+function animatieNarrow(id) {
+
+    if(id === -1) { // previous page
+
+        if(currentPageNarrow === 1) return;
+
+        pagesNarrow[--currentPageNarrow].classList.remove("flipped");
+    }
+    else { // next page
+
+        if(currentPageNarrow === numberOfPagesNarrow) return;
+
+        pagesNarrow[currentPageNarrow++].classList.add("flipped");
+    }
+}
+
+function goToPageNarrow(pageNumber) {
+
+    while(currentPageNarrow != pageNumber)
+        animatieNarrow(currentPageNarrow < pageNumber ? 1 : -1);
+}
