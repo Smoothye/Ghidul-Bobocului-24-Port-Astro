2f34eeb87efcb9f792a8467321ef0f92c7915f32
Petrut Raul <raul.petrut44@yahoo.com>
2024-09-21 22:51:26 +0300

    modificat animatia astfel incat doar un numar de pagini sunt incarcate in acelasi timp

--- public/scripts/bookAnimation.js
+++ public/scripts/bookAnimation.js
@@ -8,15 +8,68 @@ var butonCuprins = document.getElementById("buton-cuprins");
 //    SI DIN <HEAD> DIN   src/layouts/BaseLayout.astro              !!!
 var numberOfPages = 44; // numar total de pagini (ar trebui sa fie numar par) impartit la 2
 var currentPage = 1;
+var visiblePages = 6;
+
+var nextZIndex = numberOfPages;
 
 var pages = [];
+
+function loadPage(page) {
+
+    page.classList.add("active");
+
+    const imgs = page.querySelectorAll("img");
+    const frontImg = imgs[0];
+    const backImg = imgs[1];
+
+    if(frontImg && frontImg.dataset.src) {
+
+        frontImg.src = frontImg.dataset.src;
+        frontImg.removeAttribute("data-set");
+    }
+    if(backImg && backImg.dataset.src) {
+
+        backImg.src = backImg.dataset.src;
+        backImg.removeAttribute("data-set");
+    }
+}
+
+function unloadPage(page) {
+
+    page.classList.remove("active");
+
+    const imgs = page.querySelectorAll("img");
+    const frontImg = imgs[0];
+    const backImg = imgs[1];
+
+    if(frontImg && !frontImg.dataset.src) {
+
+        frontImg.dataset.src = frontImg.src;
+        frontImg.src = "";
+    }
+    if(backImg && !backImg.dataset.src) {
+
+        backImg.dataset.src = backImg.src;
+        backImg.src = "";
+    }
+}
+
 for(let i = numberOfPages; i >= 1; i--) {
     
     pages[i] = document.getElementById(`page${i}`);
     pages[i].style.zIndex = `${numberOfPages - i + 1}`;
-}
 
-var nextZIndex = numberOfPages;
+    const imgs = pages[i].querySelectorAll("img");
+
+    const frontImg = imgs.item(0);
+    const backImg = imgs.item(1);
+
+    if(i > currentPage - visiblePages && i < currentPage + visiblePages)
+        loadPage(pages[i]);
+    
+    else
+        unloadPage(pages[i]);
+}
 
 function animatie(id) { // -1 = previous page | 1 = next page
 
@@ -41,6 +94,12 @@ function animatie(id) { // -1 = previous page | 1 = next page
         pages[currentPage].classList.remove("flipped");
         pages[currentPage].style.zIndex = `${nextZIndex}`;
         nextZIndex++;
+
+        if(currentPage - visiblePages >= 1)
+            loadPage(pages[currentPage - visiblePages]);
+        
+        if(currentPage + visiblePages <= numberOfPages)
+            unloadPage(pages[currentPage + visiblePages]);
     }
     else { // next page
         if(currentPage === numberOfPages + 1) return;
@@ -63,6 +122,12 @@ function animatie(id) { // -1 = previous page | 1 = next page
             bookWrapper.style.transform = "translateX(100%)";
             nextButton.style.display = "none";
         }
+
+        if(currentPage - visiblePages >= 1)
+            unloadPage(pages[currentPage - visiblePages]);
+        
+        if(currentPage + visiblePages <= numberOfPages)
+            loadPage(pages[currentPage + visiblePages]);
     }
 
     butonCuprins.style.left = (currentPage <= 3 ? "0px" : "-100px"); // translate left buton cuprin
@@ -70,8 +135,8 @@ function animatie(id) { // -1 = previous page | 1 = next page
 
 function goToPage(pageNumber) {
 
-    // daca trebuie sa mearga mai putin de 20 de pagini se misca mai incet
-    const flipDelay = ( Math.abs(pageNumber - currentPage) < 20 ? 100 : 10 );
+    // daca trebuie sa mearga mai putin de 6 de pagini se misca mai incet
+    const flipDelay = ( Math.abs(pageNumber - currentPage) < 6 ? 100 : 10 );
 
     function flip() {
 
--- public/scripts/bookAnimationNarrow.js
+++ public/scripts/bookAnimationNarrow.js
@@ -1,18 +1,51 @@
 var prevButtonNarrow = document.getElementById("prev-narrow");
 var nextButtonNarrow = document.getElementById("next-narrow");
 
-
 //!!! NUMARUL DE PAGINI DIN public/scripts/bookAnimation.js     !!!
 //!!! NUMARUL DE PAGINI DIN  src/pages/ghid.astro               !!!
 //    SI DIN <HEAD> DIN   src/layouts/BaseLayout.astro          !!!
 var numberOfPagesNarrow = 88;
 var currentPageNarrow = 1;
+var visiblePages = 10;
 
 var pagesNarrow = [];
+
+function loadPage(page) {
+
+    page.classList.add("active");
+
+    const img = page.querySelector("img");
+
+    if(img && img.dataset.src) {
+
+        img.src = img.dataset.src;
+        img.removeAttribute("data-src");
+    }
+}
+
+function unloadPage(page) {
+
+    page.classList.remove("active");
+
+    const img = page.querySelector("img");
+
+    if(img && !img.dataset.src) {
+
+        img.dataset.src = img.src;
+        img.src = "";
+    }
+}
+
 for(let i = numberOfPagesNarrow; i >= 1; i--) {
 
     pagesNarrow[i] = document.getElementById(`page${i}-narrow`);
     pagesNarrow[i].style.zIndex = `${numberOfPagesNarrow - i + 1}`; 
+
+    if(i > currentPageNarrow - visiblePages && i < currentPageNarrow + visiblePages)
+        loadPage(pagesNarrow[i]);
+
+    else
+        unloadPage(pagesNarrow[i]);
 }
 
 function animatieNarrow(id) {
@@ -22,12 +55,24 @@ function animatieNarrow(id) {
         if(currentPageNarrow === 1) return;
 
         pagesNarrow[--currentPageNarrow].classList.remove("flipped");
+
+        if(currentPageNarrow - visiblePages >= 1)
+            loadPage(pagesNarrow[currentPageNarrow - visiblePages]);
+
+        if(currentPageNarrow + visiblePages <= numberOfPagesNarrow)
+            unloadPage(pagesNarrow[currentPageNarrow + visiblePages]);
     }
     else { // next page
 
         if(currentPageNarrow === numberOfPagesNarrow) return;
 
         pagesNarrow[currentPageNarrow++].classList.add("flipped");
+
+        if(currentPageNarrow - visiblePages >= 1)
+            unloadPage(pagesNarrow[currentPageNarrow - visiblePages]);
+
+        if(currentPageNarrow + visiblePages <= numberOfPagesNarrow)
+            loadPage(pagesNarrow[currentPageNarrow + visiblePages]);
     }
 
     prevButtonNarrow.style.display =
