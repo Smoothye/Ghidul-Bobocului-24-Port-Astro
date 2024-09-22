2346759bb3cc4916cd402a486b01b98565280a0e
Petrut Raul <raul.petrut44@yahoo.com>
2024-09-22 04:35:32 +0300

    peticit o animatie pentru sarit pagini pe ghidul cu 2 pagini

--- public/scripts/bookAnimation.js
+++ public/scripts/bookAnimation.js
@@ -8,7 +8,7 @@ var butonCuprins = document.getElementById("buton-cuprins");
 //    SI DIN <HEAD> DIN   src/layouts/BaseLayout.astro              !!!
 var numberOfPages = 44; // numar total de pagini (ar trebui sa fie numar par) impartit la 2
 var currentPage = 1;
-var visiblePages = 4; // si fata si spate
+var visiblePages = 6; // si fata si spate
 
 var nextZIndex = numberOfPages;
 
@@ -66,7 +66,7 @@ for(let i = numberOfPages; i >= 1; i--) {
         unloadPage(pages[i]);
 }
 
-function animatie(id) { // -1 = previous page | 1 = next page
+function animatie(id, loadPages=true) { // -1 = previous page | 1 = next page
 
     if(id === -1) { // previous page
         if(currentPage === 1) return;
@@ -90,11 +90,13 @@ function animatie(id) { // -1 = previous page | 1 = next page
         pages[currentPage].style.zIndex = `${nextZIndex}`;
         nextZIndex++;
 
-        if(currentPage - visiblePages >= 1)
-            loadPage(pages[currentPage - visiblePages]);
-        
-        if(currentPage + visiblePages <= numberOfPages)
-            unloadPage(pages[currentPage + visiblePages]);
+        if(loadPages) {
+            if(currentPage - visiblePages >= 1)
+                loadPage(pages[currentPage - visiblePages]);
+            
+            if(currentPage + visiblePages <= numberOfPages)
+                unloadPage(pages[currentPage + visiblePages]);
+        }
     }
     else { // next page
         if(currentPage === numberOfPages + 1) return;
@@ -109,11 +111,13 @@ function animatie(id) { // -1 = previous page | 1 = next page
             prevButton.style.display = "inline";
         }
         
-        if(currentPage - visiblePages >= 1)
-            unloadPage(pages[currentPage - visiblePages]);
-        
-        if(currentPage + visiblePages <= numberOfPages)
-            loadPage(pages[currentPage + visiblePages]);
+        if(loadPages) {
+            if(currentPage - visiblePages >= 1)
+                unloadPage(pages[currentPage - visiblePages]);
+            
+            if(currentPage + visiblePages <= numberOfPages)
+                loadPage(pages[currentPage + visiblePages]);
+        }
 
         currentPage++;
         prevButton.style.display = (currentPage === 4 ? "none" : "inline");
@@ -128,19 +132,56 @@ function animatie(id) { // -1 = previous page | 1 = next page
     butonCuprins.style.left = (currentPage <= 3 ? "0px" : "-100px"); // translate left buton cuprin
 }
 
-function goToPage(pageNumber) {
+function loadAllPages() {
+
+    for(let i = 1; i <= numberOfPages; i++) {
+
+        if(i > currentPage - visiblePages && i < currentPage + visiblePages)
+            loadPage(pages[i]);
+        
+        else
+            unloadPage(pages[i]);
+    }
+}
+async function goToPage(pageNumber) {
+    
+    const directie = (currentPage < pageNumber ? 1 : -1);
+    
+    if( Math.abs(currentPage - pageNumber) < visiblePages )
+        _goToPage(pageNumber);
+    
+    else {
+            
+        // preload 2 pagini (spatele filei din stanga si fata celei din dreapta)
+        loadPage(pages[pageNumber]);
+        loadPage(pages[pageNumber - directie]);
+        loadPage(pages[pageNumber + directie]);
+        
+        // parcurge paginile incarcate fara a incarca altele
+        await _goToPage(currentPage + visiblePages * directie, false)
+
+        while(directie * currentPage < directie * pageNumber)
+            animatie(directie, false);
+
+        await new Promise(resolve => setTimeout(resolve, 500));
+        loadAllPages();
+    }
+}
+
+async function _goToPage(pageNumber, loadPages=true) {
 
     // daca trebuie sa mearga mai putin de 6 de pagini se misca mai incet
-    const flipDelay = ( Math.abs(pageNumber - currentPage) < 6 ? 100 : 10 );
-
-    function flip() {
+    const flipDelay = ( Math.abs(pageNumber - currentPage) < 6 ? 100 : 50 );
+    
+    async function flip() {
 
         if(currentPage !== pageNumber) {
-
-            animatie(currentPage < pageNumber ? 1 : -1)
-            setTimeout(flip, flipDelay);
+            
+            animatie(currentPage < pageNumber ? 1 : -1, loadPages);
+            await new Promise(resolve => setTimeout(resolve, flipDelay));
+            return flip();
         }
     }
-
-    flip();
+    
+    await flip();
 }
